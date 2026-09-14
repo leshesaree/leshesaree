8e524721deb3672e917120995ccdcc4e301fb2a6
leshesaree <lchefness@gmail.com>
2026-09-14 14:18:12 +0530

    Load four-card homepage presentation styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./storefront-polish.css";
 import "./visual-fixes.css";
 import "./visual-v2.css";
 import "./gallery-polish.css";
+import "./home-cards.css";
 import "./account/account-polish.css";
 import "./checkout/checkout-polish.css";
 
